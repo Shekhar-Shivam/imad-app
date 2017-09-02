6f3bc173c71799352463e5a791d806b003eb3b84
Shekhar-Shivam <31345914+Shekhar-Shivam@users.noreply.github.com>
2017-09-02 21:29:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ var element = document.getElementById('main-text'
     
     //move the image
     
-    var img = document.getElementById('img');
+    var img = document.getElementById('madi');
     img.onclik = function() {
         img.style.marginLeft = '100px';
     };
