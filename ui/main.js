be250d5b0956e9d390a94f23248ec711abe1ab69
Shekhar-Shivam <31345914+Shekhar-Shivam@users.noreply.github.com>
2017-09-03 01:28:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,10 +5,12 @@ var counter=0;
 
 button.onclick = function(){
     //
-    //
+    
     //
     
-    counter = counetr+1;
+    //
+    
+    counter = counter+1;
     var span = document.getElementById('counter');
     span.innerHTML = counter.ToString();
 };
