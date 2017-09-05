d8ac369a61094ac8ea4bb1738c039f0d334793a5
Shekhar-Shivam <31345914+Shekhar-Shivam@users.noreply.github.com>
2017-09-05 21:59:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,8 @@
 // counter code
-
 var button = document.getElementById('counter');
-
 button.onclick = function(){
-    // make a request to the counter endpoint
+    
+    // create a request object
         var request = new XMLHttpRequest();
         
     //capture the reponse and store it in a variable
@@ -11,13 +10,11 @@ button.onclick = function(){
       if(request.readyState === XMLHttpRequest.DONE){
           //take some actions
           
-          if(request.status === 200)
-          {
+          if(request.status === 200)          {
                var counter = request.responseText;
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
           }
-          
       }
       // not done yet.
         
@@ -33,34 +30,29 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-    
-       // make a request to the counter endpoint
+     // create a request object
         var request = new XMLHttpRequest();
         
     //capture the reponse and store it in a variable
     request.onreadystatechange = function(){
       if(request.readyState === XMLHttpRequest.DONE){
           //take some actions
-          if(request.status === 200){
-          //capture a list of names anfd render it as a list
-          var names = request.responseText;
-          JSON.parse(names);
-          var list = '';
-          for(var i=0; i< names.length; i++)
-    {
-        list +='<li>' + names[i] + '</li>'; 
-        
+          
+          if(request.status === 200) {
+         //capture a list of names anfd render it as a list
+        var names =request.responseText;
+        names=JSON.parse(names);
+      	var list = '';
+       	for(var i=0; i< names.length; i++) {
+            list +='<li>' + names[i] + '</li>'; 
     }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-          }
-      }
+     	var ul = document.getElementById('namelist');
+    	ul.innerHTML = list;
+    }
+}
       // not done yet.
     };
-    //make request
-    request.open('GET','http://shivamshekhar96.imad.hasura-app.io/submit-name?name=' + name,true);
+    //make the request
+    request.open('GET','http://shivamshekhar96.imad.hasura-app.io/submit-name?name=' +name,true);
     request.send(null);
-    //make a request to server and get the name
-    
-    
 };
