d0306835d467ccc856f5c65352542dd8e94dda1b
Shekhar-Shivam <31345914+Shekhar-Shivam@users.noreply.github.com>
2017-09-05 22:09:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,8 +26,7 @@ button.onclick = function(){
 };
 
 //Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
      // create a request object
@@ -53,6 +52,8 @@ submit.onclick = function () {
       // not done yet.
     };
     //make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://shivamshekhar96.imad.hasura-app.io/submit-name?name=' +name,true);
     request.send(null);
 };
